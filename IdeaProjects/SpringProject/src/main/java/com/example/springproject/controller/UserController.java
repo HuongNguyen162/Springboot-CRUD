c2aa81a0b7dc23cb07eef9f02fa4751ee0f5ac04
HuongNguyen162 <lanhuonglovefamily@gmail.com>
2022-06-06 00:26:45 +0700

    Add controller logic

--- IdeaProjects/SpringProject/src/main/java/com/example/springproject/controller/UserController.java
+++ IdeaProjects/SpringProject/src/main/java/com/example/springproject/controller/UserController.java
@@ -1,13 +1,44 @@
 package com.example.springproject.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springproject.exception.ValidationRunTimeException;
+import com.example.springproject.model.Users;
+import com.example.springproject.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 
 public class UserController {
+
+    @Autowired
+    private UsersService userService;
+
+    @GetMapping("")
+    public List<Users> getUsers() {
+        return this.userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Users getUser(@PathVariable String id) {
+        return this.userService.findById(Integer.parseInt(id)).orElse(null);
+    }
+
+    @PostMapping("")
+    public Users addUser(@RequestBody Users users) {
+        return this.userService.create(users);
+    }
+
+    @PutMapping("/{id}")
+    public Users updateUser(@PathVariable String id, @RequestBody Users users) throws ValidationRunTimeException {
+        return this.userService.update(id, users);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        this.userService.delete(id);
+    }
 
 }
